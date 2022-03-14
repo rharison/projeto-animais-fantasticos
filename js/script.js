e77d3754abd199a9157c8997b21bd3e44d90ae24
rharison <rharison.abreu@gmail.com>
2022-03-14 15:22:38 -0300

    Criando estrutura para animação de números.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-import initScrollSuave from "./modules/scroll-suave.js"
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
-import initAccordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
-import initDropDownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -15,3 +16,4 @@ initModal();
 initTooltip();
 initDropDownMenu();
 initMenuMobile();
+initAnimaNumeros();
