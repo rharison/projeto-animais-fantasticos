3ae2eabaad06bfffd9665ce5aa3a2281ac112ff1
rharison <rharison.abreu@gmail.com>
2022-03-14 15:52:46 -0300

    Criando animação de números.

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,17 +1,17 @@
 export default function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
-  if(sections.length) {
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else  
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add('ativo');
+        else if (section.classList.contains('ativo')) {
           section.classList.remove('ativo');
-      })
+        }
+      });
     }
     animaScroll();
 
